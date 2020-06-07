f79a61b18b296d6b1775606f454dbbdc21554ac9
unknown <osapsilva@gmail.com>
2020-06-08 02:00:27 +0530

    Initial commit with simple controller

---
+++ src/main/java/com/example/springbbotapp/SpringbbotappApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbbotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbbotappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbbotappApplication.class, args);
+    }
+
+}
